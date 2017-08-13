b68fe5fce9ccfec802b7773e8688fb1ed7729578
Kayla Straub <kayla.straub@ngc.com>
2017-08-13 16:11:40 -0400

    Fixed error in ensemble2.  Added gitignore

--- ensemble2.py
+++ ensemble2.py
@@ -4,10 +4,13 @@ Spyder Editor
 
 This is a temporary script file.
 """
+
+import copy
 import cPickle as pkl
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import pdb
 import random
 
 from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier, AdaBoostClassifier, BaggingClassifier, VotingClassifier
@@ -22,11 +25,12 @@ from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 
 
-def clean_data(raw_data, return_labels=True):
+def process_data(raw_data, return_labels=True):
 
     # these are all the same across all employees, so remove them
     cols_to_remove = ['EmployeeCount', 'Over18', 'StandardHours', 
-                      'DailyRate', 'HourlyRate']
+                      'DailyRate', 'HourlyRate', 'MonthlyRate', 
+                      'PerformanceRating']
     for each_col in cols_to_remove:
         del raw_data[each_col]
 
@@ -41,38 +45,63 @@ def clean_data(raw_data, return_labels=True):
     raw_data.BusinessTravel.replace(travel_dict, inplace=True)
 
     # these are categorical categories, we're going to change them to binary
-    cols_to_transform = [ 'BusinessTravel', 'Department', 'EducationField',
+    cols_to_transform = [ 'Department', 'EducationField',
                           'Gender', 'JobRole', 'MaritalStatus', 'OverTime']
+    raw_data = clean_data(raw_data, cols_to_transform)
 
     data = pd.get_dummies(raw_data, columns=cols_to_transform, drop_first=True)
 
     # normalize each column
-#    data = (data - data.mean()) / (data.max() - data.min())
+    data = (data - data.mean()) / data.std()
 
     if return_labels:
         return data, labels
     else:
         return data
 
+def clean_data(data, cols):
+    """
+    Clean up the text by removing spaces and & from job titles and roles
+    """
+    for c in cols:
+        data[c] = data[c].str.replace(' ', '')
+        data[c] = data[c].str.replace('&', '')
+        data[c] = data[c].str.replace('-', '_')
+    return data
+
+
+def bootstrap(train_data, test_data, num_features=5):
+    """
+    Randomly remove some features
+    """
+    cols_to_remove = np.random.choice(train_data.columns, 5, replace=False)
+    bs_train = copy.deepcopy(train_data)
+    bs_test = copy.deepcopy(test_data)
+    for col in cols_to_remove:
+        del bs_train[col]
+        del bs_test[col]
+    return bs_train, bs_test 
+
 
 def main():
     raw_data = pd.read_csv('../train.csv')
-    data, labels = clean_data(raw_data)
+    data, labels = process_data(raw_data)
     
     raw_test = pd.read_csv('../test.csv')
-    test_data = clean_data(raw_test, return_labels=False)
+    test_data = process_data(raw_test, return_labels=False)
 
     # random forest classifier
-    RF_clf = RandomForestClassifier(max_depth=None,
-                                    random_state=0)
+    RF_clf = RandomForestClassifier(n_estimators=1000,
+				    min_samples_split=2,
+				    max_depth=None)
 
     # Decision tree classifier
     DT_clf = DecisionTreeClassifier(max_depth=None,
-                                    random_state=0)
+				    min_samples_split=2)
 
     # extra random forest classifier (best)
-    ET_clf = ExtraTreesClassifier(max_depth=None,
-                                  random_state=0)
+    ET_clf = ExtraTreesClassifier(n_estimators=1000, max_depth=None,
+				  min_samples_split=2)
 
     # naive bayes classifier
     NB_clf = GaussianNB()
@@ -83,8 +112,8 @@ def main():
                   class_weight=class_weights)
 #    clf = BaggingClassifier(base_estimator=clfi, n_estimators=50, 
 #                            max_samples=0.8, max_features=0.9, 
-#                            bootstrap=False,
-#                            bootstrap_features=False)
+#                            bootstrap=True,
+#                            bootstrap_features=True)
 
     #Nearest neighbor
     KNN_clf = KNeighborsClassifier(n_neighbors=12, weights='distance', p=3)
@@ -95,16 +124,16 @@ def main():
     # AdaBoost
     AB_clf = AdaBoostClassifier(n_estimators=90, learning_rate=0.24)
 #    clf = BaggingClassifier(base_estimator=clfi, n_estimators=50, 
-#                            max_samples=0.5, max_features=0.6, bootstrap=True,
+#                            max_samples=0.8, max_features=0.9, bootstrap=True,
 #                            bootstrap_features=True)
 
     # MLP
     MLP_clf = MLPClassifier(hidden_layer_sizes=(3,), activation='logistic')
 
-    classifiers = [SVC_clf, GP_clf, AB_clf, MLP_clf]
+    classifiers = [AB_clf, MLP_clf, SVC_clf]
     
     num_outer = 10 # change random seed each time
-    num_inner = 5  # redo train/test split
+    num_inner = 25  # redo train/test split
     clf_thresh = []
     clf_probs = []
     clf_predictions = []
@@ -116,15 +145,17 @@ def main():
         for inner_loop in xrange(num_inner):
             print("Inner: {0}".format(inner_loop))
             for clf in classifiers:
-                print("Classifier: {0}".format(clf))
+
+                # randly memove some columns (bootstrapping)
+                cv_data, bs_test_data  = bootstrap(data, test_data)
+
                 # split the data into training and testing sets
                 x_train_cv, x_test_cv, y_train_cv, y_test_cv = train_test_split(
-                    data, labels, test_size=0.05)
-    
+                    cv_data, labels, test_size=0.05)
+
                 clf.fit(x_train_cv, y_train_cv)
                 p_cv = clf.predict_proba(x_test_cv)
                 cv_score = clf.score(x_test_cv, y_test_cv)*100.0
-                print("Prediction score = %0.3f" % cv_score)
     
                 # Compute ROC curve and ROC area for each class
                 fpr, tpr, threshes = roc_curve(y_test_cv, p_cv[:, 1])
@@ -138,46 +169,70 @@ def main():
                                                                          areas[idx],
                                                                          fpr[idx], 
                                                                          tpr[idx]))
-    
-                y_pred_cv = p_cv[:,1]
-                y_pred_cv[y_pred_cv < thresh] = 0.
-                y_pred_cv[y_pred_cv >= thresh] = 1.0
-                f2, t2, _ = roc_curve(y_test_cv, y_pred_cv)
-                roc_auc2 = auc(f2, t2)
-            
-                cm = confusion_matrix(y_test_cv.astype(int), 
-                                      y_pred_cv.astype(int)).astype(np.float32)
-                cm = cm/cm.sum(axis=1)[:, np.newaxis] *100.
-                print cm
-            
-                print 'ROC area = %0.4f' % roc_auc
-                print 'ROC area (single point) = %0.4f' % roc_auc2
-    
-    
-                # run the test data through
-                test_probs = clf.predict_proba(test_data[:,0])
-                clf_probs.append(test_probs)
-    
-                test_predictions = test_probs
-                test_predictions[test_predictions < thresh] = 0.
-                test_predictions[test_predictions >= thresh] = 1.
-                clf_predictions.append(test_predictions)
+                if roc_auc > 0.6: 
+                    print("Classifier: {0}".format(clf))
+                    print("Prediction score = %0.3f" % cv_score)
+                    y_pred_cv = p_cv[:,1]
+                    y_pred_cv[y_pred_cv < thresh] = 0.
+                    y_pred_cv[y_pred_cv >= thresh] = 1.0
+                    f2, t2, _ = roc_curve(y_test_cv, y_pred_cv)
+                    roc_auc2 = auc(f2, t2)
                 
-                clf_thresh.append(thresh)
+                    cm = confusion_matrix(y_test_cv.astype(int), 
+                                          y_pred_cv.astype(int)).astype(np.float32)
+                    cm = cm/cm.sum(axis=1)[:, np.newaxis] *100.
+                    print cm
+                
+                    print 'ROC area = %0.4f' % roc_auc
+                    print 'ROC area (single point) = %0.4f' % roc_auc2
+        
+                    # run the test data through
+                    test_probs = clf.predict_proba(bs_test_data)[:,1]
+                    clf_probs.append(test_probs)
+        
+                    test_predictions = copy.deepcopy(test_probs)
+                    test_predictions[test_predictions >= thresh] = 1.
+                    test_predictions[test_predictions < thresh] = 0.
+                    clf_predictions.append(test_predictions)
+                    
+                    clf_thresh.append(thresh)
     
     pkl.dump((clf_probs, clf_predictions, clf_thresh), 
              open('ensemble_predictions.pkl', 'wb'))
-    res = pd.read_csv('../results.csv')
-    
-    final_prediction = np.mean(clf_probs, 1)
-    res = res.assign(Attrition=final_predictions)
 
-    thresh_bump = 0.
-    res['Attrition'] = pd.Series(np.where(res.Attrition.values > 1-thresh + thresh_bump, 'No', 'Yes'),
-                       res.Attrition.index)
-    res.to_csv('../SVC_AB_MLP_KNN_Probs.csv', index=False)
-    print('Threshold: {0}'.format(1-thresh+thresh_bump))
+    # print the results for a range of yes values
+    n_yes_list = [60, 65, 70, 75, 80, 85]
+    res_file_base = '../SVC_AB_MLP_Bootstrap'
+    for n_yes in n_yes_list:
+        res = pd.read_csv('../results.csv')
+        res = res.assign(Attrition=np.mean(clf_probs, 0))
+        sorted_prob = np.sort(res.Attrition.values)
+        thresh = sorted_prob[n_yes-1]
+        print("%d thresh = %0.4f" % (n_yes, thresh))
+        res['Attrition'] = pd.Series(np.where(res.Attrition.values > thresh, 'Yes', 'No'),
+                                     res.Attrition.index)
+        res.to_csv(res_file_base + '_y' + str(n_yes) + '.csv', index=False)
 
+    # Uncomment the following to take the average threshold and the median
+    # actual probability and write those to a file
+#    final_predictions = np.mean(clf_probs, 0)
+#    final_thresh = np.mean(clf_thresh)
+#    final_predictions[final_predictions >= final_thresh] = 1.
+#    final_predictions[final_predictions < final_thresh] = 0.
+#    res = res.assign(Attrition=final_predictions)
+#
+#    res['Attrition'] = pd.Series(np.where(res.Attrition.values == 1., 'Yes', 'No'),
+#                       res.Attrition.index)
+#    res.to_csv('../SVC_AB_MLP_KNN_Probs_Bootstrap.csv', index=False)
+#    
+#    
+#    final_predictions = np.round(np.median(clf_predictions, 0))
+#
+#    res = res.assign(Attrition=final_predictions)
+#    res['Attrition'] = pd.Series(np.where(res.Attrition.values == 1., 'Yes', 'No'),
+#                       res.Attrition.index)
+#    res.to_csv('../SVC_AB_MLP_KNN_Preds_Bootstrap.csv', index=False)
+#
 
 if __name__ == "__main__":
     main()
